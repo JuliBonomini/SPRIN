fc9359c518a5a4fa4818614de8b85a1ff1212b5a
Julian <julian.bonomini@gmail.com>
2017-10-03 15:40:10 -0300

    RESTfull

---
+++ src/main/java/ar/com/onready/tuto/controller/rest/ProvinciaRestController.java
@@ -0,0 +1,39 @@
+package ar.com.onready.tuto.controller.rest;
+
+import ar.com.onready.tuto.domain.Provincia;
+import ar.com.onready.tuto.service.ProvinciaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/provincias")
+public class ProvinciaRestController {
+
+    private ProvinciaService provinciaService;
+
+    public ProvinciaRestController(ProvinciaService provinciaService) {
+        this.provinciaService = provinciaService;
+    }
+
+    @GetMapping
+    public List<Provincia> buscar() {
+        return provinciaService.buscarTodas();
+    }
+
+    @GetMapping(params = "id")
+    public Provincia buscar(@RequestParam int id) {
+        return provinciaService.buscarPorId(id);
+    }
+
+    @GetMapping(params = "nombre")
+    public Provincia buscar(@RequestParam String nombre) {
+        return provinciaService.buscarPorNombre(nombre);
+    }
+
+    @PostMapping
+    public Provincia saveProvincia(@RequestBody Provincia provincia) {
+        return provinciaService.saveProvincia(provincia);
+    }
+}
